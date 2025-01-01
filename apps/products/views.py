cabd363e3fbe72ed643a3c7d7c5245228f0855b7
Saroj Paudel <sarojpaudel79@gmail.com>
2025-01-01 16:09:26 -0500

    added numpy and pandas

--- apps/products/views.py
+++ apps/products/views.py
@@ -7,6 +7,10 @@ from .serializers import ProductSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.http import HttpResponse
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from surprise import Dataset, Reader, SVD
+import pandas as pd
+from rest_framework.views import APIView
 
 # List and create products
 
@@ -47,5 +51,49 @@ class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
         if obj.created_by != self.request.user:
             raise PermissionDenied("You don't have permission to access this product")
         return obj
+
+#  Recommendations login
+
+class RecommendationAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        from yourapp.models import Rating, Product
+
+        # Get user_id from query params
+        user_id = request.query_params.get('user_id')
+
+        if not user_id:
+            # Return all products if no user_id is provided
+            products = Product.objects.all().values('id', 'item_name', 'new_price', 'description', 'image')
+            return Response(list(products))
+
+        # Fetch ratings from database
+        ratings = Rating.objects.all().values('user_id', 'item_id', 'rating')
+        df = pd.DataFrame(list(ratings))
+
+        # Train recommendation model (basic example)
+        reader = Reader(rating_scale=(1, 5))
+        data = Dataset.load_from_df(df[['user_id', 'item_id', 'rating']], reader)
+        algo = SVD()
+        trainset = data.build_full_trainset()
+        algo.fit(trainset)
+
+        # Generate recommendations for the user
+        item_ids = Product.objects.values_list('id', flat=True)
+        recommendations = [
+            {
+                "item_id": item_id,
+                "predicted_rating": algo.predict(user_id, item_id).est,
+                "item_name": Product.objects.get(id=item_id).item_name,
+                "new_price": Product.objects.get(id=item_id).new_price,
+                "description": Product.objects.get(id=item_id).description,
+                "image": Product.objects.get(id=item_id).image.url,
+            }
+            for item_id in item_ids
+        ]
+
+        # Sort recommendations by predicted rating
+        recommendations = sorted(recommendations, key=lambda x: x['predicted_rating'], reverse=True)
+
+        return Response(recommendations[:10])  # Return top 10 recommendations
 
 
